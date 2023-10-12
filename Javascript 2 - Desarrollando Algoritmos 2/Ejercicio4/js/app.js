415ce0bc15820afff049642a54e082ac62012b8f
joseptarrestoneu <joseptarrestoneu@gmail.com>
2023-10-12 13:11:06 +0200

    Arrenge Ejecicio4

--- Javascript 2 - Desarrollando Algoritmos 2/Ejercicio4/js/app.js
+++ Javascript 2 - Desarrollando Algoritmos 2/Ejercicio4/js/app.js
@@ -2,8 +2,10 @@
 // Escribe un programa con 7 variables con la temperatura de los 
 // 7 últimos días (Google). A continuación, muestra cuál ha sido la temperatura media.
 
-let temperature = [26, 27, 29, 30, 17, 20, 23];
+const ejercicio4 = () => {
+    let temperature = [26, 27, 29, 30, 17, 20, 23];
+    let tempMitja = (temperature.reduce((conc, actualValue) => (conc + actualValue),0)) / temperature.length
+    console.log(tempMitja.toFixed(2));
+}
 
-let tempMitja = (temperature.reduce((conc, actualValue) => (conc + actualValue),0)) / temperature.length
-
-console.log(tempMitja.toFixed(2));
+ejercicio4();
