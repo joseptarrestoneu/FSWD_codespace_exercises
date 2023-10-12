0d77948f87b17bad52aebf04b18ba3b0554c5dfa
joseptarrestoneu <joseptarrestoneu@gmail.com>
2023-10-12 13:09:57 +0200

    Arrenge Ejecicio3

--- Javascript 2 - Desarrollando Algoritmos 2/Ejercicio3/js/app.js
+++ Javascript 2 - Desarrollando Algoritmos 2/Ejercicio3/js/app.js
@@ -5,7 +5,17 @@
 // ****
 // ****
 
-for (let i = 0; i < 4; i++) {
-    console.log("****");
-    console.log("\n");
-}
+const ejercicio3 = () => {
+    let result = "";
+    let rows = prompt("Introduce el número de filas", 1);
+    let columns = prompt("Introduce el número de columnas", 1);
+    for (let j = 0; j < rows; j++) {
+        for (let i = 0; i < columns; i++) {
+            result += "*"
+        }
+        result += "\n"
+    }
+    console.log(result);
+};
+
+ejercicio3();
