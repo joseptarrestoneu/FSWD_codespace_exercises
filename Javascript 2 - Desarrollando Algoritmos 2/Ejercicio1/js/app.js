0ab448bc65c28afe5940ef6f5dcb7efce76ee92a
joseptarrestoneu <joseptarrestoneu@gmail.com>
2023-10-12 12:59:32 +0200

    Arrenge Ejecicio1

--- Javascript 2 - Desarrollando Algoritmos 2/Ejercicio1/js/app.js
+++ Javascript 2 - Desarrollando Algoritmos 2/Ejercicio1/js/app.js
@@ -1,9 +1,12 @@
 // Ejercicio 1
 // Escribe un programa para contar las letras en la palabra:
-// palindrome , mostrándolas una a una
+// palindrome, mostrándolas una a una
 
-let palabra = prompt("Introduce una palabra","");
+const countLetters = () => {
+    let palabra = prompt("Introduce una palabra","");
+    palabra.split("").map((element, index) => {
+        console.log(index + 1, element)
+    });
+}
 
-palabra.split("").map((element, index) => {
-    console.log(index + 1, element)
-})
+countLetters();
