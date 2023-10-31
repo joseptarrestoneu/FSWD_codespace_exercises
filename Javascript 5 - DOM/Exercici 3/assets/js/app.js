e772821dc4dd856cea803cce2bc0cd6e58a5028e
joseptarrestoneu <joseptarrestoneu@gmail.com>
2023-10-31 20:50:38 +0100

    Refactoring Exercici 3 DOM

--- Javascript 5 - DOM/Exercici 3/assets/js/app.js
+++ Javascript 5 - DOM/Exercici 3/assets/js/app.js
@@ -4,17 +4,66 @@ window.addEventListener("load", () => {
     let alert = document.querySelector(".alert");
     let close = alert.firstElementChild;
     let input = document.querySelector("#task"); 
-    // console.log(input);
+    let arrow = document.querySelector(".arrow");
+    let container = document.querySelector(".container");
+    let pencil = document.querySelector(".fa-pencil");
+    let trash = document.querySelector(".fa-trash");
     
     close.addEventListener("click", ()=> {
         alert.classList.add("dismissible")
     })
+    
     input.addEventListener("focus", () => {
         document.addEventListener("keydown", (event) => {
-            event.preventDefault(event.code === "Enter" || event.code === "NumpadEnter");
-        })
-    })
-    
+            if (event.code === "Enter" || event.code === "NumpadEnter") {
+                event.preventDefault();
+            };
+        });
+    });
+
+    arrow.addEventListener("click", (event) => {
+        // Eliminar los espacios al principio y al final del string
+        if (input.value.trim() === "") {
+            event.preventDefault();
+            input.value = "";
+            alert.classList.remove("dismissible");
+        } else {
+            id++;
+            container.querySelector("tbody")?.insertAdjacentHTML("beforeend", generateRow(id, input.value));
+            input.value = "";
+        }
+    });  
+
+    trash.addEventListener("click", (event) => {
+        event.target.
+        console.log(event);
+        
+    });
+
 });
+
+// Funcion para generar una nueva fila (nueva nota) => refactoración
+const generateRow = (id, text) => {
+    let newRow = 
+    `<tr id=${id}>
+        <td>
+            <i class='fa-solid fa-circle-check fa-2x'></i>
+            <span class='task' contenteditable='true'>${text}</span>
+        </td>
+        <td>
+            <span class='fa-stack fa-2x'>
+                <i class='fa-solid fa-square fa-stack-2x'></i>
+                <i class='fa-solid fa-pencil fa-stack-1x fa-inverse'></i>
+            </span>
+        </td>
+        <td>
+            <span class='fa-stack fa-2x'>
+                <i class='fa-solid fa-square fa-stack-2x'></i>
+                <i class='fa-solid fa-trash fa-stack-1x fa-inverse'></i>
+            </span>
+        </td>
+    </tr>`
+    return newRow
+}
 
 
